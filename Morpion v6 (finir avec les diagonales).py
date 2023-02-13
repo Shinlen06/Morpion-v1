2412ebeb545165fbf54f99f6eedc9d1a7820c636
Shinlen06 <124433317+Shinlen06@users.noreply.github.com>
2023-02-13 10:37:13 +0100

    Add files via upload

---
+++ Morpion v6 (finir avec les diagonales).py
@@ -0,0 +1,43 @@
+def dessine(plateau):
+    for ligne in plateau:
+        print()
+        for case in ligne:
+            if case == 0:
+                print(" ", end="")
+            elif case == 1:
+                print("O", end="")
+            elif case == 2:
+                print("X", end="")
+            print("|", end="")
+    print("")       
+
+
+plateau = [[0, 0, 0],
+           [0, 0, 0],
+           [0, 0, 0]]
+
+joueur = 1
+partie_continue = True
+while partie_continue:
+    dessine(plateau)
+    ligne = input("Fais le choix de la ligne entre 0 et 2 : ")
+    colonne = input("Fais le choix de la colonne entre 0 et 2 : ")
+    l = int(ligne)
+    c = int(colonne)
+    if c >= 0 and l >= 0:
+        if c < 3 and l < 3:
+            if plateau[l][c] == 0:
+                plateau[l][c] = joueur
+                if plateau[0][l] == plateau[1][l] == plateau[2][l]:
+                    print(plateau[0][l],plateau[1][l],plateau[2][l])
+                    print("Joueur vainqueur : ", joueur)
+                    dessine(plateau)
+                    partie_continue = False
+                if plateau[0][c] == plateau[1][c] == plateau[2][c]:
+                    print("Joueur vainqueur : ", joueur)
+                    dessine(plateau)
+                    partie_continue = False
+    if joueur == 1:
+        joueur = 2
+    else:
+        joueur = 1
